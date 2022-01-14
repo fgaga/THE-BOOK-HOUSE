da96a6a74cdb110e362920e127ec7a9ec84b6c3a
Fatima Garcia <fgaga2525@gmail.com>
2022-01-14 13:40:17 +0100

    BASE DE DATOS

--- BLOCK CHANGE/temp.py
+++ BLOCK CHANGE/temp.py
@@ -7,7 +7,10 @@ This is a temporary script file.
 import pandas as pd
 import csv
 
-df = pd.DataFrame([], columns = ['Libro','Autor','Sinopsis'])
-df.to_csv('libros.csv')
+df = pd.DataFrame([], columns = ['Nombre','Info','Ideautor','Foto'])
+df.to_csv('autor.csv')
 
+df = pd.DataFrame ([], columns = ['Titulo','Portada','Ideautor','Seccion', 'Pegi', 'Precio', 'Isbn','Editorial'])
+
+df.to_csv('Libros.csv')
 
