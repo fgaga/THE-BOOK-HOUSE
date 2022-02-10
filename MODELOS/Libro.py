5f5ca0dfe8c743cff530a4c34faf81b3f2407217
Fatima Garcia <fgaga2525@gmail.com>
2022-02-10 13:52:49 +0100

    modelos en el raspado

--- MODELOS/Libro.py
+++ MODELOS/Libro.py
@@ -6,33 +6,42 @@ Created on Wed Dec 29 12:54:43 2021
 @author: fatimagarcia y luisbravo
 
 """
-
-Libro.Autor()
-Libro.Libro()
+ 
 class Autor():  
-    def __init__(self,nombre,info,ideautor,foto):
+    def __init__(self,nombre,ideautor,foto):
         self.nombre = nombre
-        self.info = info
+    
         self.ideautor = ideautor
         self.foto = foto
         
     def setFoto(self,newfoto):
         self.foto = newfoto
+    def mostrarAutor(self):
+        print(50*'-')
+        print('nombre del autor',self.nombre)
+        print(50*'-')
     
 class Libro(Autor):# en los parentisis tenemos la herencia de estos dos objetos
-    def __init__(self,titulo,portada,autor,seccion,pegi,precio,isbn,editorial):
+    def __init__(self,titulo,sinopsis,autor,seccion,pegi,precio,isbn,editorial):
         self.titulo = titulo
-        self.portada = portada
+        
         self.autor = autor
         self.seccion = seccion
         self.pegi = pegi
         self.precio = precio
         self.isbn = isbn
         self.editorial = editorial
+        self.sinopsis=sinopsis
+    def mostrarLibro (self):
+        print(50*'-')
+        print ('nombre del libro',self.titulo )
+        print ('sinopsis',self.sinopsis)
+        print(50*'-')
         
     def setPrecio(self,newprecio)   :#creo una funcion set que me cambie el precio del libro automaticamente
      
          self.precio = newprecio
+         
          
          
 
--- MODELOS/untitled11.py
+++ MODELOS/untitled11.py
@@ -7,10 +7,30 @@ Created on Mon Jan 31 13:12:20 2022
 """
 
 import mysql.connector as mysql
-host= '127.0.0.128'
-bd= ''
-usuario='fatima'
+host= '200.200.200.1'
+bd= 'book_house'
+usuario='fatima' 
 password= '123456789'
 conexion= mysql.connect(host=host, database=bd,user=usuario, password=password)
 print ('CONEXION',conexion.get_server_info())
 
+"""
+
+CREATE TABLE AUTOR ( Nombre VARCHAR(255),Ideautor int, Foto varchar(100));
+CREATE TABLE libro ( Titulo varchar(255), Portada varchar (255), Autor varchar (255),
+seccion varchar(100),pegi int, precio int, ISBN varchar (100), Informacion varchar (10000) ,Editorial varchar (100));
+CREATE TABLE CLIENTE ( Nombre varchar(255), Direccion varchar (255), Email varchar (255), IdCliente int);
+
+
+
+CREATE TABLE COMPRA AS SELECT Idcliente,ISBN FROM libro,CLIENTE;
+CREATE TABLE ESCRIBE AS SELECT 	ISBN,Ideautor FROM libro,AUTOR;
+
+ 
+"""
+
+instruccion= conexion.cursor()
+try:
+     instruccion.execute("CREATE TABLE AUTOR ( Nombre VARCHAR(255),Ideautor int, Foto varchar(100));")
+except: 
+     print("ya creada")
