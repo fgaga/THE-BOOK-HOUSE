c358c901468fa920d1f2177f8688d77de8361475
luisbra <luis.bravo.collado@alumnos.upm.es>
2022-02-11 11:40:47 +0100

    Volcado de datos real . py

--- MODELOS/Libro.py
+++ MODELOS/Libro.py
@@ -8,41 +8,34 @@ Created on Wed Dec 29 12:54:43 2021
 """
  
 class Autor():  
-    def __init__(self,nombre,ideautor,foto):
+    def __init__(self,nombre,idautor):
         self.nombre = nombre
-    
-        self.ideautor = ideautor
-        self.foto = foto
+        self.info = ""
+        self.ideautor = idautor
+        self.foto = ""
+        
+    def setInfo(self,info):
+        self.info=info
         
     def setFoto(self,newfoto):
         self.foto = newfoto
-    def mostrarAutor(self):
-        print(50*'-')
-        print('nombre del autor',self.nombre)
-        print(50*'-')
-    
-class Libro(Autor):# en los parentisis tenemos la herencia de estos dos objetos
-    def __init__(self,titulo,sinopsis,autor,seccion,pegi,precio,isbn,editorial):
-        self.titulo = titulo
         
+class FichaTecnica():
+    def __init__(self): 
+        self.seccion = ""
+        self.pegi = "" 
+        self.isbn = ""
+        self.editorial = ""
+        
+class Libro(Autor,FichaTecnica):# en los parentisis tenemos la herencia de estos dos objetos
+    def __init__(self,titulo,autor,ft):
+        self.titulo = titulo
         self.autor = autor
-        self.seccion = seccion
-        self.pegi = pegi
-        self.precio = precio
-        self.isbn = isbn
-        self.editorial = editorial
-        self.sinopsis=sinopsis
-    def mostrarLibro (self):
-        print(50*'-')
-        print ('nombre del libro',self.titulo )
-        print ('sinopsis',self.sinopsis)
-        print(50*'-')
+        self.FichaTecnica=ft
         
     def setPrecio(self,newprecio)   :#creo una funcion set que me cambie el precio del libro automaticamente
      
          self.precio = newprecio
-         
-         
-         
+
 
     
